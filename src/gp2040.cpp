c37d856124ad4162b66a9e72c4ea90710705e5e7
discipleofkanye <rolostadfeldt141@gmail.com>
2026-04-11 21:02:15 +0800

    Update gp2040.cpp

--- src/gp2040.cpp
+++ src/gp2040.cpp
@@ -126,7 +126,7 @@ void GP2040::setup() {
 	addons.LoadAddon(new ReverseInput());
 	addons.LoadAddon(new TurboInput()); // Turbo overrides button states and should be close to the end
 	addons.LoadAddon(new InputMacro());
-	addons.LoadAddon(new I2CMPU6050Input(), CORE0_Input);
+	addons.LoadAddon(new I2CMPU6050Input(), CORE0_INPUT);
 
 	InputMode inputMode = gamepad->getOptions().inputMode;
 	const BootAction bootAction = getBootAction();
