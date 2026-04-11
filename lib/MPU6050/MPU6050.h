fb36a36caba597f902aecaca04fe410e63350e11
discipleofkanye <rolostadfeldt141@gmail.com>
2026-04-11 10:08:14 +0800

    Update MPU6050.h

--- lib/MPU6050/MPU6050.h
+++ lib/MPU6050/MPU6050.h
@@ -26,115 +26,30 @@
 #ifndef MPU6050_H
 #define MPU6050_H
 
-#include <BitBang_I2C.h>
+#include <stdint.h>
 
-// Used to select the gyroscope measurement range.
-// Always select the smallest possible range for precision.
-enum Mpu6050GyroscopeRange
-{
-    Max250Dps, // +/- 250 degree per second of rotational speed
-    Max500Dps,
-    Max1000Dps,
-    Max2000Dps
+// MPU6050 I2C address (default AD0 low)
+#define MPU6050_ADDR 0x68
+
+// Register map
+#define MPU6050_WHO_AM_I     0x75
+#define MPU6050_PWR_MGMT_1   0x6B
+#define MPU6050_GYRO_CONFIG  0x1B
+#define MPU6050_ACCEL_CONFIG 0x1C
+#define MPU6050_ACCEL_XOUT_H 0x3B
+#define MPU6050_GYRO_ZOUT_H  0x47
+
+class MPU6050 {
+public:
+    MPU6050();
+    bool begin(i2c_inst_t *i2c, uint8_t sda_pin, uint8_t scl_pin);
+    bool readRawAccel(int16_t &ax, int16_t &ay, int16_t &az);
+    bool readRawGyro(int16_t &gx, int16_t &gy, int16_t &gz);
+
+private:
+    i2c_inst_t *_i2c;
+    bool readRegister(uint8_t reg, uint8_t *data, uint8_t len);
+    bool writeRegister(uint8_t reg, uint8_t value);
 };
 
-// Used to select the accelerometer measurement range.
-// Always select the smallest possible range for precision.
-enum Mpu6050AccelerometerRange
-{
-    Max2g, // +/- 2g
-    Max4g,
-    Max8g,
-    Max16g
-};
-
-// Used to configure the internal Digital Low Pass Filter (DLPF)
-// The DLPF will average the measurement which is useful for slow varying
-// motion, as it removes high frequency components (such as vibrations)
-enum Mpu6050DLPFBandwidth
-{
-    Max260Hz, // 260 measurements per second
-    Max184Hz,
-    Max94Hz,
-    Max44Hz,
-    Max21Hz,
-    Max10Hz,
-    Max5Hz
-};
-
-class MPU6050
-{
-    public:
-        // Specify AD0 pin level and I2C bus when instantiating a device
-        MPU6050(int bWire, int sda, int scl, i2c_inst_t *picoI2C, int32_t speed, uint8_t addr);
-
-        // Initialize the device before reading data
-        bool init(Mpu6050AccelerometerRange accelRange = Max16g,
-                  Mpu6050GyroscopeRange gyroRange = Max2000Dps,
-                  Mpu6050DLPFBandwidth bandwidth = Max260Hz,
-                  uint8_t SampleRateDivider = 7);
-
-        void calibrateGyro();
-        void getGyroOffsets(float &x, float &y, float &z);
-        void setGyroOffsets(float x, float y, float z);
-
-        // Methods to read measurements
-        void readRawAcceleration(int16_t &rawAccelX, int16_t &rawAccelY, int16_t &rawAccelZ);
-        void readRawGyroscope(int16_t &rawGyroX, int16_t &rawGyroY, int16_t &rawGyroZ);
-        void readAcceleration(float &x, float &y, float &z); // G
-        void readGyroscope(float &x, float &y, float &z); // deg/sec
-        float readTemperature();
-
-        // Configuration of the sensors
-        void setAccelerometerRange(Mpu6050AccelerometerRange range);
-        void setGyroscopeRange(Mpu6050GyroscopeRange range);
-        void setDLPFBandwidth(Mpu6050DLPFBandwidth bandwidth);
-        void setSampleRateDivider(uint8_t divider); // sample rate = 8kHz / (1 + divider)
-
-        Mpu6050AccelerometerRange getAccelerometerRange();
-        Mpu6050GyroscopeRange getGyroscopeRange();
-        Mpu6050DLPFBandwidth getDLPFBandwidth();
-        uint8_t getSampleRateDivider();
-
-        // Resets the registers to their default value (init() should be called
-        // after a reset to use again the device)
-        void reset();
-
-        // Return true if the connection is established with the MPU6050
-        bool isConnected();
-
-        // Set sensor in sleep mode (power saving mode, no data measurements)
-        void sleepMode();
-        int wakeUp();
-
-        void set_ad0(bool ad0);
-        bool get_ad0();
-
-
-    private:
-        uint8_t address;
-	    int32_t iSpeed;
-        Mpu6050AccelerometerRange m_accelerometerRange;
-        Mpu6050GyroscopeRange m_gyroscopeRange;
-
-        BBI2C bbi2c;
-
-        float gyroOffsetX = 0;
-        float gyroOffsetY = 0;
-        float gyroOffsetZ = 0;
-
-        // Raw data to real-world units
-        float rawTemperatureToCelsius(int16_t rawTemperature);
-        float rawGyroscopeToDps(int16_t rawGyroscope); // degree/s
-        float rawAccelerationToG(int16_t rawAcceleration); // G
-
-        // For convenience:
-        uint8_t read8(uint8_t registerAddr);
-        int16_t read16(uint8_t registerAddr);
-        int write8(uint8_t registerAddr, uint8_t value);
-
-        // Buffer for I2C communication
-	    unsigned char uc[16];
-};
-
-#endif // MPU6050_H
+#endif
